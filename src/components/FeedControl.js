72dcb2d635c5c10e8a5e8d2169ce37e56a1e9d35
stevenfleming <12flemings@gmail.com>
2020-05-14 16:27:45 -0700

    creates a class component FeedControl and creates three slices of state for conditional logic for displaying forms

--- src/components/FeedControl.js
+++ src/components/FeedControl.js
@@ -1,15 +1,37 @@
 import React from "react";
 import './App.css'
 import NewMemberForm from './Member/NewMemberForm';
+import { render } from "@testing-library/react";
 
-function FeedControl() {
-  return (
-    <React.Fragment>
-      <div className="FeedControl">
-        <NewMemberForm />
-      </div>
-    </React.Fragment>
-  )
+class FeedControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      createMemberFormVisible: false,
+      createRouteFormVisible: false,
+      createSetterFormVisible: false,
+    };
+  }
+
+  setVisibleComponent = () => {
+    return (
+      <React.Fragment>
+        <div className="FeedControl">
+          <NewMemberForm />
+        </div>
+      </React.Fragment >
+    )
+  }
+
+  render() {
+    let currentView = this.setVisibleComponent();
+    return (
+      <>
+        {currentView}
+      </>
+    )
+  }
 }
+
 
 export default FeedControl;
