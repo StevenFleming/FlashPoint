5ddc603b0ae47f024a6b84a0a6e0088edcd6ba0f
stevenfleming <12flemings@gmail.com>
2020-05-17 15:04:55 -0700

    displays info about each climb on climbing Info component

--- src/components/Climb/ClimbInfo.js
+++ src/components/Climb/ClimbInfo.js
@@ -4,8 +4,11 @@ function ClimbInfo(props) {
   const { climb } = props;
   return (
     <React.Fragment>
-      <h1>You've made it to ClimbInfo!!!} </h1>
-      <p> Display this</p>
+      <h1>{climb.title} </h1>
+      <p> Grade: {climb.grade}</p>
+      <p> Incline: {climb.incline}</p>
+      <p> Gym: {climb.gym}</p>
+      <p> Setter: {climb.setter}</p>
     </React.Fragment>
   )
 }
